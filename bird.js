7e0e5c6c1ea2c20547876d20998ea011d63d0e57
VVS1306 <38920525+VVS1306@users.noreply.github.com>
2018-07-18 19:34:05 +0300

    Add files via upload

--- bird.js
+++ bird.js
@@ -1,20 +1,37 @@
 class Bird{
     // служит для создания объекта
     constructor (){
-        this.top = Math.round(500 * Math.random());
-        this.left = Math.round(500 * Math.random());
+        this.top = Math.round((500 - 48)* Math.random());
+        this.left = Math.round((500 - 48) * Math.random());
         this.color = "red";
+        this.speed = Math.round(Math.random()*4+1);
     }
     //метод выводит текущую птицу в html
     render(){
         this.div = document.createElement('div');
         this.div.setAttribute('class',`bird ${this.color}`);
+        this.div.style.top = this.top + 'px';
+        this.div.style.left = this.left + 'px';
         document.body.firstElementChild.appendChild(this.div);
         // position. left, top
     }
+    fly(){
+        var self = this;
+        setInterval(function(){
+            if(self.left>(250+Math.random()*200) || self.left<Math.random()*200){
+                self.speed*=-1;
+                if(Math.abs(self.speed)<5){
+                    self.speed*=1.5;
+                }
+            }
+            self.left+=self.speed;
+            console.log(self.left); // this теряет свой контекст когда он вызывается в новом методе (функция в функции)
+            self.div.style.top = self.top + 'px';
+            self.div.style.left = self.left + 'px';
+        },50);
+    }
 }
 
-var b = new Bird();
-    b.render();
+
 // div -прикрутить координаты
 // css - position absolut
---
+++ game.js
@@ -0,0 +1,10 @@
+var b = new Bird();
+b.render();
+b.div.onclick = shoot;
+b.fly();
+
+    function shoot(){
+        alert();
+    }
+/// Д/з
+// При шуте найти эту птичку. Установить через settimeout функцию которая будет .removeChild();
